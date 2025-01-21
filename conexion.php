5a12fd3da4d12fdaa0e62406843edd4ed30ffd6e
Franco Colavella <francocolavella1@gmail.com>
2025-01-20 23:57:20 -0300

    Tu mensaje del commit

--- conexion.php
+++ conexion.php
@@ -1,4 +1,14 @@
-   <?php
+<?php/*
         $conexion=mysqli_connect("localhost","root","","bdvmuebles") or
-        die("Problemas con la conexión");
+        die("Problemas con la conexión");*/
    ?>
+   <?php
+    // Cambia los detalles de conexión para acceder a la base de datos remota
+    $conexion = mysqli_connect(
+        "sql10.freesqldatabase.com",  // Host remoto
+        "sql10758752",               // Usuario de la base de datos
+        "4kSINUepqU",                // Contraseña de la base de datos
+        "sql10758752",               // Nombre de la base de datos
+        3306                         // Puerto (opcional, pero lo incluimos para estar seguros)
+    ) or die("Problemas con la conexión: " . mysqli_connect_error());
+?>
